518ae13fb6745cd66ea1091d4380804ac89065b4
estebanramirez13 <ecpereira@uninorte.edu.co>
2024-11-02 14:49:01 -0500

    todo listo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
     return (
-        <header className="bg-primary text-light p-4 flex justify-between items-center">
+        <header className="bg-secondary text-light p-4 flex justify-between items-center">
             <div className="flex items-center space-x-4">
                 <nav className="flex space-x-4">
-                    <a href="#configuracion" className="hover:underline text-light">Configuración</a>
-                    <a href="#clientes" className="hover:underline text-light">Clientes</a>
-                    <a href="#oportunidades" className="hover:underline text-light">Oportunidades</a>
-                    <a href="#seguimiento" className="hover:underline text-light">Seguimiento</a>
-                    <a href="#dashboard" className="hover:underline text-light">Dashboard</a>
+                    <a href="#configuracion" className="hover:underline focus:bg-primary active:bg-primary text-light p-2 rounded">Configuración</a>
+                    <a href="#clientes" className="hover:underline focus:bg-primary active:bg-primary text-light p-2 rounded">Clientes</a>
+                    <a href="#oportunidades" className="hover:underline focus:bg-primary active:bg-primary text-light p-2 rounded">Oportunidades</a>
+                    <a href="#seguimiento" className="hover:underline focus:bg-primary active:bg-primary text-light p-2 rounded">Seguimiento</a>
+                    <a href="#dashboard" className="hover:underline focus:bg-primary active:bg-primary text-light p-2 rounded">Dashboard</a>
                 </nav>
             </div>
             <div className="flex items-center justify-center flex-grow">
-                <img src="/path/to/logo.png" alt="Logo" className="h-8 w-8" />
-                <h1 className="text-xl font-bold ml-2">Universidad del Norte</h1>
+                <img src="/src/assets/logo.png" alt="Logo" className="h-14 w-auto" />
+                
             </div>
             <div className="flex items-center">
                 <span className="mr-2">Usuario: FakeUser</span>
-                <img src="/path/to/user-avatar.png" alt="User Avatar" className="h-8 w-8 rounded-full" />
+                <FontAwesomeIcon icon={faUser} className="h-8 w-8 text-light" />
             </div>
         </header>
     );
