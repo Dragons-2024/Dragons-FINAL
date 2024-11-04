69da52531d4f23fc0ce48643affbb127d7d0b170
Lena Castillo <lenac@uninorte.edu.co>
2024-11-04 18:06:06 -0500

    Fix-Button

--- src/components/WindowDialogue.tsx
+++ src/components/WindowDialogue.tsx
@@ -8,20 +8,16 @@ interface WindowDialogueProps {
 export function WindowDialogue({ open, setclose }: WindowDialogueProps) {
   return (
     <div
-      className={`fixed inset-0 flex flex-col  justify-center items-center z-10 font-poppins ${
-        open ? "visible bg-black/20" : "invisible"
-      }`}
+      className={`fixed inset-0 flex flex-col  justify-center items-center z-10 font-poppins ${open ? "visible bg-black/20" : "invisible"
+        }`}
     >
       <div className="bg-white rounded-xl shadow p-6 transition-all relative  overflow-y-auto max-h-[80vh]">
-        <button onClick={() => setclose()}>
-          <img
-            src="./src/assets/Close.svg"
-            alt="close"
-            className="absolute right-2 top-2"
-          />
+        <button onClick={setclose}>
+          <i className="fas fa-times absolute right-2 top-2 text-gray-700"></i>
         </button>
+
         <h1 className="text-3xl text-center text-[#4A5FD9]">Crear Seguimiento</h1>
-        <ActivityForm setclose={setclose}/>
+        <ActivityForm setclose={setclose} />
       </div>
     </div>
   );
