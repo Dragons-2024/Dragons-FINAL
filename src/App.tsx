b04c3cc4ca6bba3f26c34d2c360e119138b707d0
Kacueto <kacueto@uninorte.edu.co>
2024-11-12 15:29:26 -0500

    fix problem with aap.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,8 @@ import { Client } from "./core/interface/client";
 const cliente: Client = {
   id: 1,
   nombre: "Juan Pérez",
-  correoElectronico: "juan.perez@example.com",
   telefono: "123456789",
   direccion: "Calle Falsa 123",
-  fechaRegistro: "2023-01-01",
-  estado: "Activo",
   nit: "1234567890",
   ciudad: "Ciudad Ejemplo",
   pais: "País Ejemplo",
